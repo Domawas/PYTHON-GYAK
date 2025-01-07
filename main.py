97503f39a5dda2274b23e2888717dcdd68ccb4ab
Domwas <dominknagy@gmail.com>
2025-01-07 11:12:27 +0100

    feladat

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import metodus
 metodus.masodperc(3672)
+metodus.masodperc(72)
+metodus.masodperc(11502)
+
 metodus.forditva(1,1,12)
+metodus.forditva(15,12,8)
--- metodus.py
+++ metodus.py
@@ -1,19 +1,19 @@
 def masodperc(szam:int):
     
     if szam < 59:
-        print(f"{szam}mp -> 00:00:{szam:02}")
+        print(f"{szam:>4}mp -> 00:00:{szam:02}")
     elif 3600 > szam >= 60:
         
         perc = szam // 60  
         masodperc = szam % 60  
-        print(f"{szam}mp -> 00:{perc:02}:{masodperc}")  
+        print(f"{szam:>4}mp -> 00:{perc:02}:{masodperc}")  
     elif szam >= 3600:
         ora = szam // 3600  
         perc = (szam % 3600) // 60  
         masodperc = szam % 60  
-        print(f"{szam}mp -> {ora:02}:{perc:02}:{masodperc:02}")  
+        print(f"{szam:>4}mp -> {ora:02}:{perc:02}:{masodperc:02}")  
 
 
 def forditva(ora:int,perc:int,mperc:int):
     ossz = (ora * 60 * 60) + (perc * 60) + mperc
-    print(f"{ora:02}:{perc:02}:{mperc:02} -> {ossz}mp")
+    print(f"{ora:>4}ó, {perc:>4}p, {mperc:2}mp -> {ossz:4}mp")
